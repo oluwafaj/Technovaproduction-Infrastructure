40b901448cc2f12638717ed7e4640b7aa551d5ea
oluwafemi Ajao <femiajao@ymail.com>
2026-06-15 20:57:57 +0100

    Align Defender for Cloud config with existing subscription settings

--- modules/defender/main.tf
+++ modules/defender/main.tf
@@ -1,16 +1,19 @@
 # Microsoft Defender for Cloud - Free tier (CSPM)
 
 resource "azurerm_security_center_subscription_pricing" "vm" {
-  tier          = "Free"
+  tier          = "Standard"
   resource_type = "VirtualMachines"
+  subplan = "P2"
 }
 
 resource "azurerm_security_center_subscription_pricing" "storage" {
-  tier          = "Free"
+  tier          = "Standard"
   resource_type = "StorageAccounts"
+  subplan = "PerTransaction"
 }
 
 resource "azurerm_security_center_subscription_pricing" "keyvault" {
-  tier          = "Free"
+  tier          = "Standard"
   resource_type = "KeyVaults"
+  subplan = "PerTransaction"
 }
